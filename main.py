b44e7030d46b9b2a7702ad2aabcb409f59497d35
oni-97 <oni.ils.thou@gmail.com>
2022-05-05 17:35:13 +0900

    order matters

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ async def index():
     return {"Hello": "World"}
 
 
+@app.get("/sports/soccer")
+async def sport():
+    return {"sport_name": "soccer id very fun"}
+
+
 @app.get("/sports/{sport_name}")
 async def sport(sport_name: str):
     return {"sport_name": sport_name}
