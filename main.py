d9dc8c5a19ff5295405bf2a8037ae400b24902c1
oni-97 <oni.ils.thou@gmail.com>
2022-05-05 17:25:00 +0900

    path parameter

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 async def index():
     return {"Hello": "World"}
+
+
+@app.get("/sports/{sport_name}")
+async def sport(sport_name):
+    return {"sport_name": sport_name}
